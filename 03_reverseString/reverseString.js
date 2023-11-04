bc774b4229b47033414cd1d41d7a38dc777a40d5
cevher kemal sirin <cevoyou@gmail.com>
2023-11-04 15:26:58 +0300

    Solve problems 3-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    const textArray = text.split("");
+    
+    let tempText = "";
+    for(let i = 0; i < textArray.length / 2; i++)
+    {
+        tempText = textArray[i];
+        textArray[i] = textArray[textArray.length - 1 - i];
+        textArray[textArray.length-1-i] = tempText;
+    }
 
+    let newText = textArray.join("");
+    return newText;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...variables) {
+   /* for(let j = 0; j < variables.length; j++)
+    {
+        for(let i = 0; i < array.length; i++)
+        {
+            if(array[i] === variables[j])
+            {
+                array.splice(i,1);
+            }
+        }
+    }
+    return array;*/
+
+    return array.filter((item) => !variables.includes(item));
 
 };
 
