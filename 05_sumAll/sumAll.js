b450db2162a40bc372d49afc7c76b1156e29b38e
cevher kemal sirin <cevoyou@gmail.com>
2023-11-04 16:44:10 +0300

    solved problems 5-6-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    if((num1 < 0 || num2 < 0) || (typeof num1 !== "number" || typeof num2 !== "number"))
+    {
+        return "ERROR";
+    }
+
+    let number1 = Math.min(num1,num2);
+    let number2 = Math.max(num1,num2);
+
+    let total1 = ((number1 - 1) * number1) / 2;
+    let total2 = (number2 * (number2 + 1)) / 2;
+    return total2 - total1;
 
 };
 
