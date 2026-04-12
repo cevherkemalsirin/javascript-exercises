8125502cd3bc550889d72ad5386ecd021443c1d2
Cevher Kemal Sirin <76917072+cevherkemalsirin@users.noreply.github.com>
2026-04-12 22:07:13 +0300

    Solve JS Foundation 5-11

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for(let a = 0; a < args.length; a++)
+    {
+        if(arr.includes(args[a]))
+        {
+            arr.splice(arr.indexOf(args[a]),1);
+            a--;
+        }
+    
+    }
+   
+   return arr;
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if(a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b))
+        return "ERROR";
 
+    if(a > b)
+    {
+        [a,b] = [b,a];
+    }
+    let sum = 0;
+    for(;a<=b; a++)
+    {
+        sum +=a;
+    }
+    return sum;
 };
 
 // Do not edit below this line
